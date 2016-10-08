775b8359f6b1f6ffaede35e0c2b74a5e39c640c7
ed_fer <eduardojfernandezb@gmail.com>
2016-10-09 01:17:38 +0200

    implementamos el detalle de photoss

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from photos.models import Photo, VISIBILITY_PUBLIC
 
@@ -10,6 +11,24 @@ def home(request):
     :return: objeto HttpResponse con los datos de la respuesta
     """
 
-    photos = Photo.objects.filter(visibility=VISIBILITY_PUBLIC).order_by('-created_at')  # recupera todas las fotos de la base de datos
+    # recupera todas las fotos de la base de datos
+    photos = Photo.objects.filter(visibility=VISIBILITY_PUBLIC).order_by('-created_at')
     context = {'photos_list': photos[:5]}
     return render(request, 'photos/home.html', context)
+
+def photo_detail(request, pk):
+    """
+    Renderiza el detalle de una imagen
+    :param request: objeto HttpRequest con los datos de la petición.
+    :return: objeto HttpResponse con los datos de la respuesta
+    """
+    possible_photos = Photo.objects.filter(pk=pk)
+    if len(possible_photos) == 0:
+        return HttpResponseNotFound("La imagen no existe")
+    elif len(possible_photos) > 1:
+        return HttpResponse("Múltiples opciones", status=300)
+
+    photo = possible_photos[0]
+    context = {'photo': photo}
+
+    return render(request, 'photos/photo_detail.html', context)
