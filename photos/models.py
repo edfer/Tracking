6488a7f50b0df80d8c58ee12543fa4bcf96eb444
ed_fer <eduardojfernandezb@gmail.com>
2016-10-08 22:23:02 +0200

    Refactorizamos el modelo Photo haciendo constantes los valores del campo license. Y se crea el campo owner y visibility

--- photos/models.py
+++ photos/models.py
@@ -1,23 +1,41 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+LICENSE_COPYRIGHT = 'RIG'
+LICENSE_COPYLEFT = 'LEF'
+LICENSE_CC = 'CC'
+
+
+LICENSES = (
+     (LICENSE_COPYRIGHT, 'Copyright'),
+     (LICENSE_COPYLEFT, 'Copyleft'),
+     (LICENSE_CC, 'Creative Commons')
+)
+
+VISIBILITY_PUBLIC = 'PUB'
+VISIBILITY_PRIVATE = 'PRI'
+
+VISIBILITY =(
+    (VISIBILITY_PUBLIC, 'Pública'),
+    (VISIBILITY_PRIVATE, 'Privadapython manage.py')
+)
+
 class Photo(models.Model):
 
-    LICENSES = (
-        ('RIG', 'Copyright'),
-        ('LEF', 'Copyleft'),
-        ('CC', 'Creative Commons')
-    )
 
+    owner = models.ForeignKey(User)
     name = models.CharField(max_length=150)
     url = models.URLField()
-    description = models.TextField()
-    license = models.CharField(max_length=3, choices=LICENSES)
+    description = models.TextField(null=True, blank=True)
+    license = models.CharField(max_length=3, choices=LICENSES, default=LICENSE_CC)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+    visibility = models.CharField(max_length=3, choices=VISIBILITY, default=VISIBILITY_PUBLIC)
 
     # Definimos el nombre del modelo de la app!
-    # lo que se ve en el admin cuando entramos en el modelo registrado en admin.py
+    # el nombre que se ve en el admin cuando entramos en el modelo registrado en admin.py
 
     def __str__(self):
         return self.name
